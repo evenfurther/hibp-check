4ae2d74006dfba7bf731a35ffd36e14a96ee35e3
Samuel Tardieu <sam@rfc1149.net>
2021-10-25 23:21:59 +0200

    Add padding to HIBP requests

--- src/network.rs
+++ src/network.rs
@@ -15,7 +15,11 @@ lazy_static! {
 /// a given prefix.
 pub async fn hibp_network_request(prefix: &str) -> Result<String, Error> {
     let url = format!("{}/{}", HIBP_BASE, prefix);
-    let response = CLIENT.get(&url).send().await?;
+    let response = CLIENT
+        .get(&url)
+        .header("Add-Padding", "true")
+        .send()
+        .await?;
     ensure!(
         response.status().is_success(),
         "received error status code for {}: {}",
